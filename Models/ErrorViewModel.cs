cec67dad26352c0ccff31385c6c5deae99659f0f
Reynier González <39437340+ReynierGo@users.noreply.github.com>
2023-03-09 06:38:34 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSHARP_TEST1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
